f5915b0cdd5ceda332700f7657b31789bff8ce86
CJYuanDa <changjy8832@gmail.com>
2023-11-07 10:14:15 +0800

    Sqrt(x)

---
+++ Sqrt(x)/Sqrt(x).js
@@ -0,0 +1,13 @@
+let x = 8;
+
+// My solution
+function mySqrt(x) {
+    return Math.floor(x ** (1/2));
+}
+
+// My solution
+function mySqrt1(x) {
+    return Math.floor(Math.sqrt(x));
+}
+
+console.log(mySqrt(x));
